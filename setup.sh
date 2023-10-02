bc178b5ab7d329aeeb19e1fc84b5eecc0c2d8de7
Stella <122531435+intrastella@users.noreply.github.com>
2023-10-02 23:41:39 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python3 -m pip install -e ./tabular_search_engine
-cd ./preview_model
+cd ./tabular_search_engine
 pip3 install openai
 pip3 uninstall pinecone-client
 pip3 install pinecone-client[grpc]
